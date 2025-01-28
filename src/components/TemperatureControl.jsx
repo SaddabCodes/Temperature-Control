a4031767b2109e969e0e40b929947ca08144e3e2
Saddab <saddabakhtar17@gmail.com>
2025-01-28 23:13:39 +0530

    update code

--- src/components/TemperatureControl.jsx
+++ src/components/TemperatureControl.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
 import "./temperatureControl.css";
 
 export default function TemperatureControl() {
+  const [temperatureValue, setTemperatureValue] = useState(0);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
+  const increaseTemprature = () => {
+    if (temperatureValue === 30) return;
+
+    const newTemperature = temperatureValue + 1;
+    if (newTemperature >= 15) {
+      setTemperatureColor("hot");
+    }
+    setTemperatureValue(newTemperature);
+  };
+
+  const decreaseTemprature = () => {
+    if (temperatureValue === 0) return;
+
+    const newTemperature = temperatureValue - 1;
+    if (newTemperature < 15) {
+      setTemperatureColor("cold");
+    }
+    setTemperatureValue(newTemperature);
+  };
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${temperatureColor}`}>
+          {temperatureValue}°C
+        </div>
+      </div>
+
+      <div className="button-container">
+        <button onClick={increaseTemprature}>+</button>
+        <button onClick={decreaseTemprature}>-</button>
+      </div>
     </div>
   );
 }
